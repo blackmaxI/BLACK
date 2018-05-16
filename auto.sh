0367ba8452b46759d55e0cdfb1082eafa446a4a9
blackmaxI <38557194+blackmaxI@users.noreply.github.com>
2018-05-16 17:18:33 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,5 +1,5 @@
 while true ; do
-./launch.sh
+screen ./launch.sh
 rm -rf ~/.telegram-cli/$entry/data/animation/*
 rm -rf ~/.telegram-cli/$entry/data/audio/*
 rm -rf ~/.telegram-cli/$entry/data/document/*
@@ -12,7 +12,7 @@ rm -rf ~/.telegram-cli/$entry/data/thump/*
 rm -rf ~/.telegram-cli/$entry/data/video/*
 rm -rf ~/.telegram-cli/$entry/data/voice/*
 
-curl "https://api.telegram.org/bot587847161:AAE-Z5PimBF-gS6z2dCXqtDITTXVmWsqBSA/sendMessage" -F "chat_id=-1001374162505" -F "text=Mehdi_Yt Your Self Bot Crashed And Was Ready To Work :D"
+curl "https://api.telegram.org/bot464371613:AAFhsIv0yzCb4NKf-yfyIzK-L9Me3cAdERA/sendMessage" -F "chat_id=-1001135197265" -F "text=Your Self Bot Crashed And Was Ready To Work :D"
 
 sleep 8
 done
